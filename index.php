48eb0cd307f64bdb1656136fd346bf523b08cb09
YaroslavSavele <asis174@mail.ru>
2021-07-26 11:38:35 +0500

    Изменяет массив на ассоциативный и исправляет кавычки

--- index.php
+++ index.php
@@ -54,51 +54,59 @@ $user_name = 'Ярослав'; // укажите здесь ваше имя
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
         <?php
-        $categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+        $categories = [
+           "boards" => "Доски и лыжи",
+           "attachment" => "Крепления",
+           "boots" => "Ботинки",
+           "clothing" => "Одежда",
+           "tools" => "Инструменты",
+           "other" => "Разное"
+         ];
+
         $goods = [
            [
-              'title' => '2014 Rossignol District Snowboard',
-              'category' => 'Доски и лыжи',
-              'price' => 10999,
-              'image' => 'img/lot-1.jpg'
+              "title" => "2014 Rossignol District Snowboard",
+              "category" => $categories["boards"],
+              "price" => 10999,
+              "image" => "img/lot-1.jpg"
            ],
            [
-              'title' => 'DC Ply Mens 2016/2017 Snowboard',
-              'category' => 'Доски и лыжи',
-              'price' => 159999,
-              'image' => 'img/lot-2.jpg'
+              "title" => "DC Ply Mens 2016/2017 Snowboard",
+              "category" => $categories["boards"],
+              "price" => 159999,
+              "image" => "img/lot-2.jpg"
            ],
            [
-              'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-              'category' => 'Крепления',
-              'price' => 8000,
-              'image' => 'img/lot-3.jpg'
+              "title" => "Крепления Union Contact Pro 2015 года размер L/XL",
+              "category" => $categories["attachment"],
+              "price" => 8000,
+              "image" => "img/lot-3.jpg"
            ],
            [
-              'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
-              'category' => 'Ботинки',
-              'price' => 	10999,
-              'image' => 'img/lot-4.jpg'
+              "title" => "Ботинки для сноуборда DC Mutiny Charocal",
+              "category" => $categories["boots"],
+              "price" => 	10999,
+              "image" => "img/lot-4.jpg"
            ],
            [
-              'title' => 'Куртка для сноуборда DC Mutiny Charocal',
-              'category' => 'Одежда',
-              'price' => 7500,
-              'image' => 'img/lot-5.jpg'
+              "title" => "Куртка для сноуборда DC Mutiny Charocal",
+              "category" => $categories["clothing"],
+              "price" => 7500,
+              "image" => "img/lot-5.jpg"
            ],
            [
-              'title' => 'Маска Oakley Canopy',
-              'category' => 'Разное',
-              'price' => 5400,
-              'image' => 'img/lot-6.jpg'
+              "title" => "Маска Oakley Canopy",
+              "category" => $categories["other"],
+              "price" => 5400,
+              "image" => "img/lot-6.jpg"
            ],
         ];
         ?>
         <ul class="promo__list">
             <!--заполните этот список из массива категорий-->
-            <?php foreach ($categories as $val): ?>
-            <li class="promo__item promo__item--boards">
-                <a class="promo__link" href="pages/all-lots.html"><?=$val;?></a>
+            <?php foreach ($categories as $key => $category): ?>
+            <li class="promo__item promo__item--<?=$key;?>">
+                <a class="promo__link" href="pages/all-lots.html"><?=$category;?></a>
             </li>
             <?php endforeach; ?>
         </ul>
@@ -111,18 +119,18 @@ $user_name = 'Ярослав'; // укажите здесь ваше имя
         
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-            <?php foreach ($goods as $val): ?>
+            <?php foreach ($goods as $key => $good): ?>
             <li class="lots__item lot">
                 <div class="lot__image">
-                    <img src="<?=$val['image'];?>" width="350" height="260" alt="">
+                    <img src="<?=$good["image"];?>" width="350" height="260" alt="">
                 </div>
                 <div class="lot__info">
-                    <span class="lot__category"><?=$val['category'];?></span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$val['title'];?></a></h3>
+                    <span class="lot__category"><?=$good["category"];?></span>
+                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$good["title"];?></a></h3>
                     <div class="lot__state">
                         <div class="lot__rate">
                             <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost"><?=$val['price'];?><b class="rub">р</b></span>
+                            <span class="lot__cost"><?=$good["price"];?><b class="rub">р</b></span>
                         </div>
                         <div class="lot__timer timer">
                             12:23
