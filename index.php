de036224c57303f8f096384259c3c6755806be84
YaroslavSavele <asis174@mail.ru>
2021-08-09 17:35:51 +0500

    Чтение и показ записей из MySQL

--- index.php
+++ index.php
@@ -3,6 +3,30 @@ require_once("helpers.php");
 require_once("functions.php");
 require_once("data.php");
 
+$con = mysqli_connect("localhost", "newuser", "", "yeticave");
+if (!$con) {
+   $error = mysqli_connect_error();
+} else {
+   $sql = "SELECT character_code, name_category FROM categories";
+   $result = mysqli_query($con, $sql);
+      if($result) {
+        $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+      } else {
+        $error = mysqli_error($con);
+      }
+}
+
+$sql = "SELECT lots.title, lots.start_price, lots.img, lots.date_finish, categories.name_category FROM lots
+JOIN categories ON lots.category_id=categories.id
+WHERE date_creation > '2021-07-15' ORDER BY date_creation DESC";
+
+$res = mysqli_query($con, $sql);
+if ($res) {
+   $goods = mysqli_fetch_all($res, MYSQLI_ASSOC);
+} else {
+    $error = mysqli_error($con);
+  }
+
 $page_content = include_template("main.php", [
    "categories" => $categories,
    "goods" => $goods
